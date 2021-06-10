addc16c8d3f9a6caf5e6f3ed741d1f96e2a21189
Josh Thompson <thompsonjoshd@gmail.com>
2021-06-10 07:21:15 -0600

    2nd test done

--- code/test/poodr_ch_6_test.rb
+++ code/test/poodr_ch_6_test.rb
@@ -5,6 +5,7 @@ require './poodr_ch6_01.rb'
 
 class BicycleTest < Minitest::Test
   def test_attributes
+    skip
     bike = Bicycle.new(size: 'M', tape_color: 'red')
 
     spares = { 
@@ -15,6 +16,25 @@ class BicycleTest < Minitest::Test
       
     assert_equal 'M', bike.size 
     assert_equal 'red', bike.tape_color
+    assert_equal spares, bike.spares
+  end
+  
+  def test_mountain_bike_attributes
+    bike = Bicycle.new(
+      style: :mountain,
+      size: 'S',
+      front_shock: 'Manitou',
+      rear_shock: 'Fox'
+    )
+    
+    spares = {
+      tire_size: "2.1",
+      chain: "10-speed",
+      rear_shock: 'Fox'
+    }
+    assert_equal :mountain, bike.style
+    assert_equal 'Manitou', bike.front_shock
+    assert_equal 'Fox', bike.rear_shock
     assert_equal spares, bike.spares
   end
 end
