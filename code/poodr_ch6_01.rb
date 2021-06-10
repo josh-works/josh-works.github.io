d58da037496910e577c220d087115b35232de792
Josh Thompson <thompsonjoshd@gmail.com>
2021-06-10 07:24:06 -0600

    wip

--- code/poodr_ch6_01.rb
+++ code/poodr_ch6_01.rb
@@ -1,17 +1,29 @@
 # class Bicycle, p 107
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :size, :tape_color, :style, :front_shock, 
+              :rear_shock
   
   def initialize(args)
+    @style = args[:style]
     @size = args[:size]
     @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
   
   def spares
-    { chain: '10-speed',
-      tire_sice: '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      { chain: '10-speed',
+        tire_sice: '23',
+        tape_color: tape_color
+      }
+    else
+      {
+        chain: '10-speed',
+        tire_size: '2.1',
+        rear_shock: rear_shock
+      }
+    end
   end
 end
 
